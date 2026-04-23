ea6d68f80e24ef64103593e4b12265ca47da2e68
Ryan Waltemate <waltemry@oregonstate.edu>
2026-04-22 20:54:51 -0700

    added race pos UI element, does nothing

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,7 @@ public class UIManager : MonoBehaviour
     public ShipManager playerShipManager;
     public Slider chargeSlider;
     public TextMeshProUGUI speedText;
+    public TextMeshProUGUI racePos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
