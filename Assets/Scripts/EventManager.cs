365a0cf10cc24193d220f5c7f33b078076d616c0
Ryan Waltemate <waltemry@oregonstate.edu>
2026-04-30 14:28:48 -0700

    renamed script and moved scripts in inspector

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SceneLoader : MonoBehaviour
+public class EventManager : MonoBehaviour
 {
     public RacerSpawner racerSpawner;
     public RacePositionTracker RPT;
